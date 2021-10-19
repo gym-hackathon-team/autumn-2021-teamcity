b4c67062707579e6f3c4e9925d6d4339298ce3da
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2021-10-20 00:12:35 +0300

    Changed -a to -e in start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ if [ ! -d ssl ] ; then
   mkdir ssl
 fi
 
-if [ ! -a ssl/ssl.crt ] || [ ! -a ssl/ssl.key ] ; then
+if [ ! -e ssl/ssl.crt ] || [ ! -e ssl/ssl.key ] ; then
   rm -f ssl/ssl.crt ssl/ssl.key
   openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out ssl/ssl.crt -keyout ssl/ssl.key -batch
 fi
 
-if [ ! -a ssl/dhparam.pem ] ; then
+if [ ! -e ssl/dhparam.pem ] ; then
   openssl dhparam -out ssl/dhparam.pem 2048
 fi
 
