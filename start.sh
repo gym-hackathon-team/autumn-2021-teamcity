c0e66a1f356c20bd8a71be94537f74d249d97897
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2021-10-19 23:50:03 +0300

    Removed ssl keys

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d ssl ] ; then
+  mkdir ssl
+fi
+
+if [ ! -a ssl/ssl.crt ] || [ ! -a ssl/ssl.key ] ; then
+  rm -f ssl/ssl.crt ssl/ssl.key
+  openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out ssl/ssl.crt -keyout ssl.key -batch
+fi
+
+if [ ! -a ssl/dhparam.pem ] ; then
+  openssl dhparam -out ssl/dhparam.pem 2048
+fi
+
+GID="$(cut -d: -f3 < <(getent group docker))"
+GID=${GID} docker-compose up -d
