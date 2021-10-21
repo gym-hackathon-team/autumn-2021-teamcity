e0650f79dffe6305b7c60b85f79a13544fbe99b8
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2021-10-21 19:51:51 +0300

    Installed Flutter

--- build-agents/build.sh
+++ build-agents/build.sh
@@ -4,3 +4,8 @@ cd android || exit
 docker build . -t jastenewname/android-build-agent
 docker push jastenewname/android-build-agent
 cd ..
+
+cd flutter || exit
+docker build . -t jastenewname/flutter-build-agent
+docker push jastenewname/flutter-build-agent
+cd ..
