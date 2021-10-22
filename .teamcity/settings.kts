9b99d20ce0a805e88c6797d36fd2fdffd3f6589d
test <test@TeamCity>
2021-10-22 07:02:57 +0000

    TeamCity change in 'Autumn 2021 Backend' project: runners of 'Deploy' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -305,6 +305,15 @@ object Autumn2021Backend_Deploy : BuildType({
             param("secure:jetbrains.buildServer.deployer.password", "credentialsJSON:ab3f9123-62ae-46d5-9bad-32848580053b")
             param("jetbrains.buildServer.sshexec.authMethod", "PWD")
         }
+        step {
+            name = "Update configs"
+            type = "ssh-exec-runner"
+            param("jetbrains.buildServer.deployer.username", "root")
+            param("jetbrains.buildServer.sshexec.command", "cd")
+            param("jetbrains.buildServer.deployer.targetUrl", "5.63.154.19")
+            param("secure:jetbrains.buildServer.deployer.password", "credentialsJSON:ab3f9123-62ae-46d5-9bad-32848580053b")
+            param("jetbrains.buildServer.sshexec.authMethod", "PWD")
+        }
     }
 
     triggers {
